b180b5709ed982fc71ba5fa9201b64c166024f3b
julioBarbosa33 <barbosajulio131@gmail.com>
2024-06-21 10:52:53 -0300

    Task State

---
+++ TaskState/TaskState/Data/StateDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StateAPI.State;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace StateAPI.Data
+{
+    public class StateDBContext : DbContext
+    {
+        public StateDBContext(DbContextOptions<StateDBContext> options) : base(options) { }
+        public DbSet<TasksModel> TasksModel { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TasksModel>().Property(t => t.State);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TaskState/TaskState/State/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿using StateAPI.State.Enum;
+
+namespace StateAPI.State
+{
+    public class TasksModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public StateTask State { get; set; }
+    }
+}
